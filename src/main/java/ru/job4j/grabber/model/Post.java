ebf92188c0d69e499db68f5b5fc61d041b61c979
Dmitriy Kupreev <d3_october@yahoo.com>
2025-04-01 17:09:50 +0300

    Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -0,0 +1,87 @@
+package ru.job4j.grabber.model;
+
+import java.util.Objects;
+
+public class Post {
+    private Long id;           // Идентификатор вакансии
+    private String title;      // Название вакансии
+    private String link;       // Ссылка на описание вакансии
+    private String description; // Описание вакансии
+    private Long time;
+
+    public Post(Long id, String title, String link, String description, Long time) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id.equals(post.id) && link.equals(post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, link);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", link='" + link + '\'' +
+                ", description='" + description + '\'' +
+                ", time=" + time +
+                '}';
+    }
+}
