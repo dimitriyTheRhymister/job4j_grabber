dd29f4a3b7b67b297ddf8eac9ef00bb8e0e8eadd
Dmitriy Kupreev <d3_october@yahoo.com>
2025-04-06 21:45:36 +0300

    Web [#289478]

---
+++ src/main/java/ru/job4j/grabber/service/WebApp.java
@@ -0,0 +1,100 @@
+package ru.job4j.grabber.service;
+
+import io.javalin.Javalin;
+import org.apache.log4j.Logger;
+import ru.job4j.grabber.stores.JdbcStore;
+import ru.job4j.grabber.stores.Store;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class WebApp {
+    private static final Logger LOGGER = Logger.getLogger(WebApp.class);
+
+    private static LocalDateTime convertMillisToLocalDateTime(long millis) {
+        Instant instant = Instant.ofEpochMilli(millis);
+        return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+    }
+
+    public static void main(String[] args) throws SQLException {
+        var config = new Config();
+        config.load("src/main/resources/config.properties");
+        String url = config.get("db.url");
+        String username = config.get("db.user");
+        String password = config.get("db.password");
+        int interval = Integer.parseInt(config.get("parser.interval"));
+        int port = Integer.parseInt(config.get("server.port"));
+
+        Connection connection;
+        LOGGER.info("1 successfully");
+        System.out.println(url + username + password);
+
+        connection = DriverManager.getConnection(url, username, password);
+        Store dbStore = new JdbcStore(connection);
+        LOGGER.info("2 successfully");
+
+        SchedulerManager schedulerManager = new SchedulerManager();
+        schedulerManager.init();
+        schedulerManager.load(
+                interval,
+                SuperJobGrab.class,
+                dbStore);
+        LOGGER.info("3 successfully");
+
+        Javalin app = Javalin.create().start(port);
+        LOGGER.info("Сервер запущен на порту 7000");
+
+        app.get("/", ctx -> {
+            LOGGER.info("Запрос получен на /");
+            ctx.result("Welcome to the Simple App!");
+        });
+
+        app.get("/test", ctx -> {
+            LOGGER.info("Запрос получен на /test");
+            ctx.result("Test endpoint is working");
+        });
+
+        app.get("/jobs", ctx -> {
+            LOGGER.info("Запрос получен на /jobs");
+            ctx.result("Jobs endpoint is working");
+
+            var jobsFromDb = dbStore.getAll();
+
+            StringBuilder html = new StringBuilder();
+            html.append("<!DOCTYPE html>")
+                    .append("<html lang=\"ru\">")
+                    .append("<head>")
+                    .append("<meta charset=\"UTF-8\">")
+                    .append("<title>Available Jobs</title>")
+                    .append("</head>")
+                    .append("<body>")
+                    .append("<h1>Available Jobs:</h1>")
+                    .append("<hr>")
+                    .append("<ol>");
+
+            for (var job : jobsFromDb) {
+                html.append("<li>")
+                        .append(job.getTitle())
+                        .append(" - ")
+                        .append("<a href=\"")
+                        .append(job.getLink())
+                        .append("\">")
+                        .append(job.getLink())
+                        .append("</a>")
+                        .append(" - ")
+                        .append(convertMillisToLocalDateTime(job.getTime()))
+                        .append("</li>");
+            }
+
+            html.append("</ol>")
+                    .append("</body>")
+                    .append("</html>");
+
+            ctx.html(html.toString());
+        });
+    }
+}
